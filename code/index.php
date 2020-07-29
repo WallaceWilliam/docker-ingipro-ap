3cbe54b3aa0208f6b5f814391bb531b928f70989
WallaceWilliam <volos4@ukr.net>
2020-07-29 15:59:47 +0300

    modified:   README.md 	new file:   code/.htaccess 	new file:   code/index.php 	new file:   docker-compose.yml 	new file:   log/ap/access.log 	new file:   log/ap/error.log 	new file:   log/ap/other_vhosts_access.log 	new file:   log/ng/access.log 	new file:   log/ng/error.log 	new file:   log/ng/web-access.log 	new file:   log/ng/web-error.log 	new file:   unicum 	new file:   web.conf

---
+++ code/index.php
@@ -0,0 +1,14 @@
+<?php
+if (!empty($_SERVER['HTTP_CLIENT_IP'])) {
+    $ip = $_SERVER['HTTP_CLIENT_IP'];
+    echo "HTTP_CLIENT_IP ";
+} elseif (!empty($_SERVER['HTTP_X_FORWARDED_FOR'])) {
+    $ip = $_SERVER['HTTP_X_FORWARDED_FOR'];
+    echo "HTTP_X_FORWARDED_FOR ";
+} else {
+    $ip = $_SERVER['REMOTE_ADDR'];
+    echo "REMOTE_ADDR ";
+}
+echo $ip . "\n";
+echo phpinfo();
+?>
